c175d8da0ef4dff8aff38dda62eacce1e95b7efa
SahilSSB <sahilstudios32@gmail.com>
2026-07-14 19:13:26 +0530

    add, sub, mult, div

--- vm.c
+++ vm.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "chunk.h"
 #include "common.h"
 #include "debug.h"
 #include "value.h"
@@ -30,6 +31,12 @@ Value pop() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+  do { \
+    double b = pop(); \
+    double a = pop(); \
+    push(a op b); \
+  } while (false);
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -50,9 +57,13 @@ static InterpretResult run() {
                         return INTERPRET_OK;
                       }
       case OP_NEGATE: {
-                        push(-pop());
+                        *(vm.stackTop - 1)= -*(vm.stackTop - 1);
                         break;
                       }
+      case OP_ADD:      BINARY_OP(+); break;
+      case OP_SUBTRACT: BINARY_OP(-); break;
+      case OP_MULTIPLY: BINARY_OP(*); break;
+      case OP_DIVIDE:   BINARY_OP(/); break;
       case OP_CONSTANT: {
                           Value constant = READ_CONSTANT();
                           push(constant);
@@ -61,6 +72,7 @@ static InterpretResult run() {
                         }
     }
   }
+#undef BINARY_OP
 #undef READ_BYTE
 #undef READ_CONSTANT
 }
