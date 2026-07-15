5bdfa1893709fe3eea1e656d1d589685f4c63978
SahilSSB <sahilstudios32@gmail.com>
2026-07-15 16:32:32 +0530

    scanner

--- vm.c
+++ vm.c
@@ -4,6 +4,7 @@
 #include "debug.h"
 #include "value.h"
 #include "vm.h"
+#include "compiler.h"
 
 VM vm;
 
@@ -77,8 +78,7 @@ static InterpretResult run() {
 #undef READ_CONSTANT
 }
 
-InterpretResult interpret(Chunk* chunk) {
-  vm.chunk = chunk;
-  vm.ip = vm.chunk->code;
-  return run();
+InterpretResult interpret(const char* source) {
+  compile(source);
+  return INTERPRET_OK;
 }
